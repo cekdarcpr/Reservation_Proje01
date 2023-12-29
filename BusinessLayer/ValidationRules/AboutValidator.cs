5568a89351b767f80beb91898028c622d54f83e1
cekdarcpr <cekooo12@gmail.com>
2023-12-29 15:11:15 +0300

    #14 changes

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator() 
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmı boş bırakılamaz...");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen görsel seçiniz");
+            RuleFor(x => x.Description).MinimumLength(20).WithMessage("En az 20 karakterli açıklama yazınız...");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("En fazla 1500 karakterli açıklama yazabilirsiniz");
+        }
+
+    }
+}
---
+++ Reservation_Proje01/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reservation_Proje01.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
