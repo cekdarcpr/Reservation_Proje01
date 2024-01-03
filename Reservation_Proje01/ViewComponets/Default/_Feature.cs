54dbf6d8fd0984445c46ea7e75c9f960f7d203f0
cekdarcpr <cekooo12@gmail.com>
2024-01-03 20:19:14 +0300

    #18

---
+++ Reservation_Proje01/ViewComponets/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reservation_Proje01.ViewComponets.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.GetList();
+
+            return View(values);
+        }
+    }
+}
---
+++ Reservation_Proje01/ViewComponets/Default/_Statistics.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reservation_Proje01.ViewComponets.Default
+{
+    public class _Statistics : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            using var _context = new Context();
+            ViewBag.v1 = _context.Destinations.Count();
+            ViewBag.v2 = _context.Guides.Count();
+            ViewBag.v3 = "285";
+            return View(); 
+        
+        }
+    }
+}
