c91cdc543821bf966a721b1c413169dee49cf96c
cekdarcpr <cekooo12@gmail.com>
2024-01-26 14:47:30 +0300

    #31son

--- Reservation_Proje01/Program.cs
+++ Reservation_Proje01/Program.cs
@@ -1,3 +1,9 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Reservation_Proje01.Models;
+
 namespace Reservation_Proje01
 {
     public class Program
@@ -7,7 +13,21 @@ namespace Reservation_Proje01
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<Context>();
+            builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>()
+            .AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
             builder.Services.AddControllersWithViews();
+           
+            builder.Services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            builder.Services.AddMvc();
+            
 
             var app = builder.Build();
 
@@ -21,7 +41,7 @@ namespace Reservation_Proje01
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
