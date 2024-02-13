5fa1a24e66173942716275de3fc777783094530d
cekdarcpr <cekooo12@gmail.com>
2024-02-13 22:26:50 +0300

    #36Son

--- Reservation_Proje01/Controllers/LoginController.cs
+++ Reservation_Proje01/Controllers/LoginController.cs
@@ -10,13 +10,15 @@ namespace Reservation_Proje01.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-		public LoginController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-		[HttpGet]
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
@@ -55,6 +57,23 @@ namespace Reservation_Proje01.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.UserName, p.Password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Profile",new { area = "Member"});
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
             return View();
         }
     }
---
+++ Reservation_Proje01/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reservation_Proje01.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Kullanıcı adınızı giriniz...")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz...")]
+        public string Password { get; set; }
+    }
+}
